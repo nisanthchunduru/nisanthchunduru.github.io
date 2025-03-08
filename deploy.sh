c42fe002097ee66f90110bc55f1f1eb02325181f
Nisanth Chunduru <chunisan@amazon.com>
2025-03-08 19:04:06 +0530

    Modify docker-compose.production.yml to start hugo-notion

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 npm run docker:push:x86
+scp .env root@150.136.2.200:/opt/blog/.env
 scp docker-compose.production.yml root@150.136.2.200:/opt/blog/docker-compose.yml
 ssh root@150.136.2.200 "cd /opt/blog && docker compose down"
 ssh root@150.136.2.200 "cd /opt/blog && docker compose up -d --pull always"
